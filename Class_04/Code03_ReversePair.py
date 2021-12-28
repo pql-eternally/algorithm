e6dc279522c3a92b1d23f94c29b86c14e427dda0
庞启龙 <1223750634@qq.com>
2021-12-28 11:05:16 +0800

    init class 04 code

--- Class_04/Code03_ReversePair.py
+++ Class_04/Code03_ReversePair.py
@@ -1,3 +1,81 @@
 """
 在一个数组中，任何一个前面的数a，和任何一个后面的数b，如果(a,b)是降序的，就称为降序对。给定一个数组arr，求数组的降序对总数量
 """
+import copy
+import random
+
+
+def generate_random_arr(max_size: int = 20, min_value: int = 0, max_value: int = 100) -> list:
+    """
+    生成随机数组
+    """
+    size = random.randint(1, max_size)
+    arr = []
+    for i in range(0, size):
+        num = random.randint(min_value, max_value)
+        arr.append(num)
+    return arr
+
+
+def reverse_pair(arr: list) -> int:
+    return process(arr, 0, len(arr) - 1)
+
+
+def process(arr: list, left: int, right: int) -> int:
+    if left == right:
+        return 0
+    mid = left + ((right - left) >> 1)
+    return process(arr, left, mid) + process(arr, mid + 1, right) + merge(arr, left, mid, right)
+
+
+def merge(arr: list, left: int, mid: int, right: int):
+    """
+    合并两个有序列表
+    """
+    p1 = left
+    p2 = mid + 1
+    help_arr = []
+    res = 0
+    while p1 <= mid and p2 <= right:
+        # 左侧数值小，拷贝左侧
+        if arr[p1] <= arr[p2]:
+            help_arr.append(arr[p1])
+            p1 += 1
+        # 右侧数值小，拷贝右侧，拷贝右侧时说明此处是一个逆序对，查看左侧有多少数字比当前数大
+        else:
+            help_arr.append(arr[p2])
+            res += mid - p1 + 1
+            p2 += 1
+    help_arr.extend(arr[p1: mid + 1])
+    help_arr.extend(arr[p2: right + 1])
+    arr[left: right + 1] = help_arr
+    return res
+
+
+def comparator(arr: list):
+    res = 0
+    for i in range(0, len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[i]:
+                res += 1
+    return res
+
+
+def main():
+    print('Start ...')
+    test_count = 10000
+    for i in range(0, test_count):
+        arr = generate_random_arr(10)
+        arr1 = copy.copy(arr)
+        arr2 = copy.copy(arr)
+        res1 = reverse_pair(arr1)
+        res2 = comparator(arr2)
+        print(f'arr: {arr}，res1：{res1}，res2：{res2}')
+        if res1 != res2:
+            print(f'arr: {arr}，res1：{res1}，res2：{res2}')
+            return
+    print('Done ...')
+
+
+if __name__ == '__main__':
+    main()
--- Class_04/Code04_BiggerThanRightTwice.py
+++ Class_04/Code04_BiggerThanRightTwice.py
@@ -9,3 +9,82 @@
 2的后面没有
 所以总共有5个
 """
+
+import copy
+import random
+
+
+def bigger_twice(arr: list) -> int:
+    return process(arr, 0, len(arr) - 1)
+
+
+def process(arr: list, left: int, right: int) -> int:
+    if left == right:
+        return 0
+    mid = left + ((right - left) >> 1)
+    return process(arr, left, mid) + process(arr, mid + 1, right) + merge(arr, left, mid, right)
+
+
+def merge(arr: list, left: int, mid: int, right: int) -> int:
+    res = 0
+    # 统计当前merge操作中左侧数字是右侧数字两倍的个数
+    window_r = mid + 1
+    for i in range(left, mid + 1):
+        while window_r <= right and arr[i] > (arr[window_r] << 1):
+            window_r += 1
+        res += window_r - mid - 1
+
+    p1 = left
+    p2 = mid + 1
+    help_arr = []
+    while p1 <= mid and p2 <= right:
+        if arr[p1] <= arr[p2]:
+            help_arr.append(arr[p1])
+            p1 += 1
+        else:
+            help_arr.append(arr[p2])
+            p2 += 1
+    help_arr.extend(arr[p1: mid + 1])
+    help_arr.extend(arr[p2: right + 1])
+    arr[left: right + 1] = help_arr
+    return res
+
+
+def generate_random_arr(max_size: int = 20, min_value: int = 0, max_value: int = 100) -> list:
+    """
+    生成随机数组
+    """
+    size = random.randint(1, max_size)
+    arr = []
+    for i in range(0, size):
+        num = random.randint(min_value, max_value)
+        arr.append(num)
+    return arr
+
+
+def comparator(arr: list):
+    res = 0
+    for i in range(0, len(arr)):
+        for j in range(i + 1, len(arr)):
+            if (arr[j] << 1) < arr[i]:
+                res += 1
+    return res
+
+
+def main():
+    print('Start ...')
+    test_count = 10000
+    for i in range(0, test_count):
+        arr = generate_random_arr(100)
+        arr1 = copy.copy(arr)
+        arr2 = copy.copy(arr)
+        res1 = bigger_twice(arr1)
+        res2 = comparator(arr2)
+        if res1 != res2:
+            print(f'arr: {arr}，res1：{res1}，res2：{res2}')
+            return
+    print('Done ...')
+
+
+if __name__ == '__main__':
+    main()
