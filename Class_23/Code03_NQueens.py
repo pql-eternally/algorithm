95dc41734c0b8e81b2a876793805adbcaddd7693
庞启龙 <1223750634@qq.com>
2022-02-17 15:50:12 +0800

    N皇后问题暴力递归版本

--- Class_23/Code03_NQueens.py
+++ Class_23/Code03_NQueens.py
@@ -10,7 +10,34 @@ from typing import List
 
 
 class Solution:
-    def num(self, N: int):
+    def queen(self, N: int):
+        # 记录每行存放的皇后位置
+        record = [0] * N
+        return self.process(N, 0, record)
+
+    def process(self, N: int, index: int, record: List):
+        if index == N:
+            return 1
+        ways = 0
+        for col in range(N):
+            if self.validate_conflict(record, index, col):
+                record[index] = col
+                ways += self.process(N, index + 1, record)
+        return ways
+
+    def validate_conflict(self, record: List[int], index: int, col: int):
+        for i in range(index):
+            # 校验列/左下对角线/右下对角线是否冲突
+            if col == record[i] or abs(record[i] - col) == abs(index - i):
+                return False
+        return True
+
+
+class Solution2:
+    def queen(self, N: int):
+        """
+        使用位运算实现
+        """
         pass
 
 
@@ -18,7 +45,7 @@ def main():
     n = 4
     t1 = time.time()
     solution = Solution()
-    res1 = solution.num(n)
+    res1 = solution.queen(n)
     t2 = time.time()
     print(t2 - t1, res1)
 
