a35ed985988a04617336957ae1a8ad04609f99e1
庞启龙 <1223750634@qq.com>
2022-03-09 11:10:29 +0800

    init code

---
+++ Class_26/Code04_StepProblem.py
@@ -0,0 +1,16 @@
+"""
+一个人可以一次往上迈1个台阶，也可以迈2个台阶，返回迈上N级台阶的方法数
+"""
+
+
+class Solution:
+    def step(self, n: int) -> int:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ Class_26/Code06_CeramicMethodCount.py
@@ -0,0 +1,16 @@
+"""
+用1*2的瓷砖，把N*2的区域填满，返回铺瓷砖的方法数
+"""
+
+
+class Solution:
+    def count(self, s: str) -> int:
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
