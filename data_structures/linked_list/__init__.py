c80bc9c1d6ae8959b9a2fa3671ae70f812fe5307
pql <1223750634@qq.com>
2023-01-29 10:13:14 +0800

    init linked list

--- data_structures/linked_list/__init__.py
+++ data_structures/linked_list/__init__.py
@@ -8,13 +8,43 @@ Nodes contain data and also may link to other nodes:
     - Head Node: First node, the address of the
                  head node gives us access of the complete list
     - Last node: points to null
+
+各个py说明
+
+单链表：
+from_sequence：从序列生成链表
+print_reverse：链表反转输出
+middle_element_of_linked_list：取链表中间元素
+swap_nodes：链表交换节点
+circular_linked_list：环形链表
+has_loop：链表是否有环
+is_palindrome：是否回文链表
+singly_linked_list：单链表
+merge_two_lists：合并两个链表变成有序链表
+
+双链表：
+doubly_linked_list：双链表
+doubly_linked_list_two：双链表
+deque_doubly：双端队列
+
+其它：
+skip_list：跳表，参考自 https://epaperpress.com/sortsearch/download/skiplist.pdf
 """
 from typing import Any, List
 
 
 class Node:
-    def __init__(self, item: Any, next: Any) -> None:
-        self.item = item
+
+    def __init__(self, data: Any, next: Any = None) -> None:
+        self.data = data
+        self.next = next
+
+
+class DoubleNode:
+
+    def __init__(self, data: Any, pre: Any = None, next: Any = None):
+        self.data = data
+        self.pre = pre
         self.next = next
 
 
@@ -23,11 +53,11 @@ class LinkedList:
         self.head: Node | None = None
         self.size = 0
 
-    def add(self, item: Any) -> None:
+    def add(self, data: Any) -> None:
         """
         往链表头部添加元素
         """
-        self.head = Node(item, self.head)
+        self.head = Node(data, self.head)
         self.size += 1
 
     def remove(self) -> Any:
@@ -37,10 +67,10 @@ class LinkedList:
         if self.is_empty():
             return None
         else:
-            item = self.head.item
+            data = self.head.data
             self.head = self.head.next
             self.size -= 1
-            return item
+            return data
 
     def is_empty(self) -> bool:
         return self.head is None
@@ -60,7 +90,7 @@ class LinkedList:
         item_list: List[str] = []
         node = self.head
         while node:
-            item_list.append(str(node.item))
+            item_list.append(str(node.data))
             node = node.next
         return " --> ".join(item_list)
 
