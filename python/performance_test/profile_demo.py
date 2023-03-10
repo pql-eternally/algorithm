7191f67381f6464429f5daefa855dc0e48d651ea
pql <1223750634@qq.com>
2023-03-10 13:51:09 +0800

    update cprofile demo

--- python/performance_test/profile_demo.py
+++ python/performance_test/profile_demo.py
@@ -1,8 +1,12 @@
 """
 line_profiler可以用来分析代码的行执行时间
 kernprof -lv profile_demo.py
+
+测试一下代码总的效率以及各个部分的效率
+python -m cProfile profile_demo.py
 """
 import cProfile
+import functools
 
 
 def is_prime(num):
@@ -43,4 +47,57 @@ ncalls  tottime  percall  cumtime  percall filename:lineno(function)
     1    0.000    0.000    0.269    0.269 {built-in method builtins.exec}
     1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 """
-cProfile.run('list(PrimeIter(10000))')
+
+
+# cProfile.run('list(PrimeIter(10000))')
+
+
+def fib(n: int):
+    """
+     ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+4356587/31    1.213    0.000    1.213    0.039 profile_demo.py:55(fib)
+     31/1    0.000    0.000    1.214    0.607 profile_demo.py:68(fib_seq)
+
+    """
+    if n <= 2:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+
+@functools.lru_cache()
+def fib_with_cache(n):
+    """
+     ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        31    0.000    0.000    0.000    0.000 profile_demo.py:61(fib_with_cache)
+        31/1    0.000    0.000    0.000    0.000 profile_demo.py:68(fib_seq)
+    """
+    if n <= 2:
+        return 1
+    return fib_with_cache(n - 1) + fib_with_cache(n - 2)
+
+
+def fib_with_dp(n):
+    if n < 1:
+        return 0
+    dp = [1, 1]
+    for i in range(2, n):
+        dp.append(dp[i - 1] + dp[i - 2])
+    return dp
+
+
+def fib_seq(n: int, fib_fn: callable):
+    """
+    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.000    0.000    0.000    0.000 profile_demo.py:79(fib_with_dp)
+    """
+    res = []
+    if n >= 1:
+        res.extend(fib_seq(n - 1, fib_fn))
+    res.append(fib_fn(n))
+    return res
+
+
+if __name__ == '__main__':
+    # fib_seq(30, fib)
+    # fib_seq(30, fib_with_cache)
+    fib_with_dp(30)
